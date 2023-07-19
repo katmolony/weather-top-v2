c816c09ee222bde90601a4c691e5a0e991bd188f
Kate Molony <katemolony96@gmail.com>
2023-07-19 19:07:07 +0100

    Add Playlist Controller, editted json file

--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { dashboardController } from "./controllers/dashboard-controller.js";
 import { aboutController } from "./controllers/about-controller.js";
+import { stationController } from "./controllers/station-controller.js";
 
 export const router = express.Router();
 
@@ -9,3 +10,5 @@ router.get("/dashboard", dashboardController.index);
 router.get("/about", aboutController.index);
 
 router.post("/dashboard/addstation", dashboardController.addStation);
+
+router.get("/station/:id", stationController.index);

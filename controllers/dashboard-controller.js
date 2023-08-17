63616847705b33df7623ab644bd07dbb0e93411a
Kate Molony <katemolony96@gmail.com>
2023-08-17 17:34:28 +0100

    stations in alphabetical order

--- controllers/dashboard-controller.js
+++ controllers/dashboard-controller.js
@@ -1,12 +1,23 @@
 import { stationStore } from "../models/station-store.js";
 import { accountsController } from "./accounts-controller.js";
+import { stationAnalytics } from "../utils/station-analytics.js";
+import { readingStore } from "../models/reading-store.js";
 
 export const dashboardController = {
   async index(request, response) {
     const loggedInUser = await accountsController.getLoggedInUser(request);
+    const stations = await stationStore.getStationsByUserId(loggedInUser._id);
+    //sort list of stations in alphabetical order
+    const sortedList = stations.sort((a, b) =>
+    a.name.localeCompare(b.name));
+    //  for (const station of stations) {
+    //    const latestReading = stationAnalytics.getLatestReading(station);
+    //    station.latestReading = latestReading;
+    //    }
+
     const viewData = {
       title: "Station Dashboard",
-      stations: await stationStore.getStationsByUserId(loggedInUser._id),
+      stations: sortedList,
     };
     console.log("dashboard rendering");
     response.render("dashboard-view", viewData);
